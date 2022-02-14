9a80023555ae9dbb21aaf5a43c984bbb13cf1823
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-02-14 20:15:14 +0600

    practising arguments

--- arguments.js
+++ arguments.js
@@ -1,7 +1,12 @@
 function addNumbers(num1, num2) {
-    const result = num1 + num2;
+    console.log(arguments);
+    let result = 0;
+    for (const num of arguments) {
+        result = result + num;
+    }
+    // const result = num1 + num2;
     return result;
 }
 
-const sum = addNumbers(23, 13);
+const sum = addNumbers(23, 13, 50, 100,420);
 console.log(sum);
