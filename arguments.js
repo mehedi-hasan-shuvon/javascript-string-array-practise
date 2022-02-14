bf2435002fd1740c0ceca1aba23db843cf0d035b
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-02-14 20:21:02 +0600

    practising arguments

--- arguments.js
+++ arguments.js
@@ -8,5 +8,16 @@ function addNumbers(num1, num2) {
     return result;
 }
 
-const sum = addNumbers(23, 13, 50, 100,420);
+const sum = addNumbers(23, 13, 50, 100, 420);
 console.log(sum);
+
+function getFullName(firstname, lastname) {
+    let fullname = "";
+    for (const part of arguments) {
+        fullname = fullname + part + " ";
+    }
+
+    return fullname;
+}
+const name = getFullName('omuk', 'sonket', 'bin', 'hanif', 'shonket');
+console.log(name);
