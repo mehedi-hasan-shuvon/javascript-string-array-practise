6a90293104e64c15976505af75cbf33758e7c497
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-02-14 21:19:24 +0600

    added date timming

---
+++ date.js
@@ -0,0 +1,18 @@
+const myFavDate = new Date('1971-12-16');
+console.log(myFavDate);
+
+//another way to setup time
+
+const anotherDate = new Date(1971, 3, 26, 11, 50, 40, 80);
+console.log(anotherDate);
+
+const str1 = 'Bangladesh is a beautiful country.';
+console.log(str1.endsWith("country"));
+const str = 'I love coding more than eating.';
+
+const names = ["rafiq", "jabbar", "bakkar", "habib", "dalim"];
+console.log(names.reverse())
+
+const array = ["121", "12", "1", "112", "111"];
+
+console.log(array.sort());
